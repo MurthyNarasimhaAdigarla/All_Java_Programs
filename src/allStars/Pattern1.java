9482737e9375c4c6df4f5c05d82175dedd2285ee
murthy.adigarlaa@gmail.com <murthy.adigarlaa@gmail.com>
2019-10-24 11:04:44 +0530

    changes

--- src/allStars/Pattern1.java
+++ src/allStars/Pattern1.java
@@ -1,19 +1,16 @@
 package allStars;
 
-public class Stars {
+public class Pattern1 {
 
 	public static void main(String args[]) {
 
 		int i, j;
 
 		//rows
-		for (i = 0; i < 5; i++) {
-
+		for (i = 1; i <= 5; i++) {
 			//Cols
-			for (j = 0; j <= i; j++) {
-
+			for (j = 1; j <= i; j++) {
 				System.out.print("*");
-
 			}
 			System.out.println(" ");
 
